842fc4e3d32b4715ad6cf61502584eb4ed544820
Andrey Mironov <andreesssen@gmail.com>
2023-07-06 21:38:58 +0300

    добавлено editor.js

--- app/src/components/editor/Index.jsx
+++ app/src/components/editor/Index.jsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { useAppSelector } from '@/src/hooks/useRedux'
-import { Editor } from './Editor'
-import './editor.scss'
+import { useAppSelector } from '@/src/hooks/useRedux';
+import dynamic from 'next/dynamic';
+import './editor.scss';
+
+const EditorJs = dynamic(import('./Editor'), {
+  ssr: false,
+});
 
 export const EditorComponent = () => {
   const { isOpen } = useAppSelector(store => store.editor)
 
   return (
     <div className={isOpen ? 'editor open' : 'editor'}>
-      <div className="editor__wrapper">{isOpen && <Editor />}</div>
+      <div className="editor__wrapper">{isOpen && <EditorJs />}</div>
     </div>
   )
 }
